d7c53a4879e9ec2651920454123f4d249b97919b
Ítalo Bezerra <italo.bezerra@dbserver.com.br>
2025-09-09 08:29:51 -0300

    refactor(pauta-resultado): utiliza map tipado para mensagens e adiciona verificação de valor nulo

--- frontend-desafio/src/app/modules/detalhes-pauta/pipes/pauta-resultado.pipe.ts
+++ frontend-desafio/src/app/modules/detalhes-pauta/pipes/pauta-resultado.pipe.ts
@@ -1,24 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 import { IPautaDetalhes } from '../interfaces/pauta-detalhes.interface';
+import { ResultadoPautaType } from '@app/shared/types/resultao-pautas.type';
 
 @Pipe({
   name: 'pautaResultado',
 })
 export class PautaResultadoPipe implements PipeTransform {
   public transform(value: IPautaDetalhes): string {
-    if (value.resultado === 'aprovado') {
-      return `Sessão encerrada. Pauta aprovada com ${value.porcentagem_sim}% dos votos`;
+    if (!value) {
+      return '';
     }
 
-    if (value.resultado === 'anulada') {
-      return `Sessão encerrada. Pauta anulada por falta de votos.`;
-    }
+    const MENSAGEM_MAP: Record<ResultadoPautaType, string> = {
+      aprovado: `Sessão encerrada. Pauta aprovada com ${value.porcentagem_sim}% dos votos`,
+      anulada: `Sessão encerrada. Pauta anulada por falta de votos.`,
+      empate: `Sessão encerrada. Pauta não aprovada por empate de votos.`,
+      reprovado: `Sessão encerrada. Pauta reprovada com ${value.porcentagem_nao}% de votos`,
+    };
 
-    if (value.resultado === 'empate') {
-      return `Sessão encerrada. Pauta não aprovada por empate de votos.`;
-    }
-
-    return `Sessão encerrada. Pauta reprovada com ${value.porcentagem_nao}% de votos`;
+    return MENSAGEM_MAP[value.resultado] 
+      ?? 'Sessão encerrada. Resultado desconhecido';
   }
 }
